88b1f9ef1663e013b6c2565d47a06ca451a54819
Санек <84853777+magrega@users.noreply.github.com>
2023-06-05 19:58:20 +0300

    Update delete-comments.js
    Updated the script to work with current LinkedIn

--- delete-comments.js
+++ delete-comments.js
@@ -1,63 +1,80 @@
-// https://www.linkedin.com/in/{username}/detail/recent-activity
+//https://www.linkedin.com/in/{username}/recent-activity/comments/
 
 /**
  * Halts execution for a certain amount of seconds.
  * https://stackoverflow.com/a/39914235
+ * 
  */
-function sleep(seconds) {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-}
 
-// Gets Comment dropdowns on comments you've authored.
-function getDeleteCommentDropdowns() {
-    var dropdowns = [], buttons = [];
-    for (const dropdown of document.querySelectorAll(".artdeco-dropdown__trigger.artdeco-dropdown__trigger--placement-bottom.ember-view.comment-options-trigger.t-black--light.m0")) {
-        dropdowns.push(dropdown)
-    }
-    return dropdowns;
-}
+
+let keepGoing = true;
+
+const sleep = seconds => new Promise(resolve => setTimeout(resolve, seconds * 1000));
 
 // Gets "Delete" button inside "Are you sure you want to delete your comment?" confirmation box.
-function getDeleteConfirmationButton() {
-    return document.querySelector("button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
-}
+const getDeleteConfirmationButton = () => document.querySelector("button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view");
 
-//
-async function deleteComment() {
+// Clicks confirmation button in delete modal window
+const deleteComment = async () => {
     await sleep(2);
-
-    var deleteConfirmationButton = getDeleteConfirmationButton();
+    const deleteConfirmationButton = getDeleteConfirmationButton();
     if (deleteConfirmationButton) {
         deleteConfirmationButton.click();
+    } else {
+        console.log("error getting confirm button");
     }
 }
 
-//
-async function deleteActivity() {
-    var deleteDropdowns = getDeleteCommentDropdowns();
-    for (var i = 0; i < deleteDropdowns.length; i++) {
-        deleteDropdowns[i].click()
-        spans = document.querySelectorAll(".comment-options-dropdown__option-text span")
-        for (const span of spans) {
-            if (span.textContent.includes("Delete")) {
-                span.click();
-            }
-        }
-        deleteComment();
-        await sleep(3);
-    }
+// Deletes comment
+const deleteActivity = async (comment) => {
+    comment.closest('.comments-post-meta').parentElement.lastElementChild.querySelector('button').click();
+    console.log('clicked dropdown button');
+    await sleep(2);
+    comment.closest('.comments-post-meta').parentElement.lastElementChild.querySelector('button').nextElementSibling.querySelectorAll('.single-line')[2].firstElementChild.click();
+    console.log('clicked delete button');
+    deleteComment();
+    await sleep(3);
+    console.log('deleted comment');
 }
 
-//
-var keepGoing = true;
-async function init() {
+// Makes cycle wait until iteration finishes
+const makeLoopAwait = (comment) => {
+    return new Promise(resolve => {
+        setTimeout(() =>
+            resolve(deleteActivity(comment)), 5000);
+    })
+}
+
+// Starts the whole thing
+const init = async () => {
     console.log("*** Starting activity deletion ***");
-    console.log(">>> Deleting comments")
-    deleteActivity();
+
+    const allComments = document.getElementsByClassName("comments-post-meta__name-text");
+
+    for (let i = 0; i < allComments.length; i++) {
+        if (allComments[i].textContent.includes(commentAuthor) && allComments[i].closest('.comments-comment-item__post-meta')) {
+            console.log('Deleting base comment');
+            console.log(allComments[i]);
+            await makeLoopAwait(allComments[i]);
+        }
+    }
+
+    for (let i = 0; i < allComments.length; i++) {
+        if (allComments[i].textContent.includes(commentAuthor) && allComments[i].closest('.comments-reply-item__post-meta')) {
+            console.log('Deleting reply comment');
+            console.log(allComments[i]);
+            await makeLoopAwait(allComments[i]);
+        }
+    }
+
+    window.scrollTo(0, document.body.scrollHeight);
+
+    console.log('ended init() iteration, scrolling down');
     if (keepGoing) {
-        await sleep(5);
+        await sleep(4);
         init();
     }
 }
 
+const commentAuthor = document.querySelector('.single-line-truncate').innerText;
 init();
